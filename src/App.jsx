3bc94be6119cb381d61b5e238ccafb3f3e205898
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-09 17:17:01 +0200

    feat: add second useState  and prevent default event

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,29 @@ import boardgames from "./data/boardgames";
 
 function App() {
 
-  const [items, setItems] = useState('');
+  // useState iniziale --> array di oggetti (prodotti)
+  const [items, setItems] = useState(boardgames);
+  // useState all'aggiunta del nuovo prodotto
+  const [newItem, setItem] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log('Aggiunto il prodotto ' + newItem)
+  }
 
   return (
     <>
       {/* // Lista dei prodotti */}
+
       <ul>
         {boardgames.map(boardgame => <li key={boardgame.id}>{boardgame.title}</li>)}
       </ul>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
-          value={items}
-          onChange={e => { setItems(e.target.value) }}
+          value={newItem}
+          onChange={e => { setItem(e.target.value) }}
         />
       </form>
     </>
