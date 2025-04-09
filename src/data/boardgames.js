06469e07278a935274220569c170d769a764181f
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-09 15:47:10 +0200

    feat: create articles (boardgames) array

---
+++ src/data/boardgames.js
@@ -0,0 +1,35 @@
+const boardgames =
+  [
+    {
+      id: 1,
+      title: 'Catan',
+      description: "Un gioco di strategia e risorse in cui i giocatori cercano di costruire e sviluppare insediamenti, città e strade su una mappa modulare.",
+      category: "Strategia"
+    },
+    {
+      id: 2,
+      title: "Ticket to Ride",
+      description: "Un gioco di avventura ferroviaria in cui i giocatori collezionano e giocano carte treno per rivendicare tratte ferroviarie che collegano città in tutto il Nord America (o altre mappe).",
+      category: "Strategia leggera",
+    },
+    {
+      id: 3,
+      title: "Dixit",
+      description: "Un gioco di narrazione creativa con carte illustrate in modo evocativo. Un giocatore seleziona una carta dalla propria mano e fornisce un indizio (una parola, una frase, un suono). Gli altri giocatori scelgono una carta dalla propria mano che si adatta all'indizio.",
+      category: "Festa"
+    },
+    {
+      id: 4,
+      title: "7 Wonders",
+      description: "Un gioco di civilizzazione a turni simultanei in cui i giocatori guidano una delle sette grandi città del mondo antico. Ogni giocatore costruisce meraviglie e sviluppa la propria civiltà attraverso risorse, strutture commerciali, militari e scientifiche.",
+      category: "Strategia"
+    },
+    {
+      id: 5,
+      title: "Pandemic",
+      description: "Un gioco cooperativo in cui i giocatori lavorano insieme come specialisti nella lotta contro quattro malattie mortali che si stanno diffondendo in tutto il mondo.",
+      category: "Cooperativo"
+    }
+  ]
+
+export default boardgames;
