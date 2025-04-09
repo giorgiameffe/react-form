ccf529b5094a61dd1026dc5e37ea5148b1a54c91
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-09 15:53:03 +0200

    feat: complete first milestone

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import boardgames from "./data/boardgames";
+
 function App() {
 
 
   return (
     <>
-
+      <ul>
+        {boardgames.map(boardgame => <li>{boardgame.title}</li>)}
+      </ul>
     </>
   )
 }
 
-export default App
+export default App;
