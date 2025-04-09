a7175715a3face6634ac6ba25617ef0ddda10a53
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-09 16:29:47 +0200

    feat: create articles form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+
 import boardgames from "./data/boardgames";
 
 function App() {
 
+  const [items, setItems] = useState('');
 
   return (
     <>
+      {/* // Lista dei prodotti */}
       <ul>
-        {boardgames.map(boardgame => <li>{boardgame.title}</li>)}
+        {boardgames.map(boardgame => <li key={boardgame.id}>{boardgame.title}</li>)}
       </ul>
+
+      <form>
+        <input
+          type="text"
+          value={items}
+          onChange={e => { setItems(e.target.value) }}
+        />
+      </form>
     </>
   )
 }
